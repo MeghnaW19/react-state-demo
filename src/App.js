2dfea26e0c4cb77b4a4465e188fb89bcbaed90d8
Ximanta <ximanta.sarma@gmail.com>
2021-07-08 20:50:42 +0530

    Demo Code 2 for React 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.css";
 import ParentComponent from "./components/ParentComponent";
 
 function App() {
-  const message = "Message from App component to Parent Component";
-
-  return (
+   return (
     <div className="App">
       <h1>Hello! I am the Root Component</h1>
       <ParentComponent></ParentComponent>
-      <ParentComponent message={message}></ParentComponent>
+
 
     </div>
   );
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -2,48 +2,35 @@ import React from 'react';
 import ChildComponent1 from './ChildComponent1';
 import ChildComponent2 from './ChildComponent2';
 
-
 class ParentComponent extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);  
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ value: event});
   }
-  onSubmit(event) {
-    alert(this.state.value);
-  }
-
+  
   render() {
     return (
       <div>
-        <h2>I am the Parent component</h2>
-        <p>{this.props.message}</p>
-        <p>{this.state.inputMessage}</p>
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <div>
-              <label>Input 1</label>
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-              <p>State value: {this.state.value}</p>
-            </div>
-            <div>
-              <button type="submit" value="Submit">Get Input</button>
-            </div>
-          </div>
-        </form>
-        <ChildComponent1 input = {this.state.value}></ChildComponent1>
-        <ChildComponent2 input = {this.state.value}></ChildComponent2>
+        <h2>I am the Parent component</h2>      
+        <p>State value: {this.state.value}</p>        
+       <table>
+         <tr>
+         
+     <td width="30%"> <ChildComponent1 input = {this.state.value} handleChangeRef={this.handleChange}></ChildComponent1></td>
+        <td width="40%"></td>
+
+      <td width="30%"><ChildComponent2 input = {this.state.value}></ChildComponent2></td> 
+        </tr>
+        </table>
+      
       </div >
     );
   }
 }
 
 export default ParentComponent;
-
-
